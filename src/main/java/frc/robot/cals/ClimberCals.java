9b46773947c8e3aa3ffddc9985b9b4dc209c1c70
Roboprog <roboprog@BishopFoley.org>
2024-02-12 19:39:38 -0500

    undisabled some subsystems and updated librarys

--- src/main/java/frc/robot/cals/ClimberCals.java
+++ src/main/java/frc/robot/cals/ClimberCals.java
@@ -4,10 +4,9 @@ import frc.robot.subsystems.motor.MotorCal;
 import frc.robot.subsystems.motor.MotorCal.TypeMotor;
 
 public class ClimberCals {
-    public boolean disable = false;
+    public boolean disable = true;
 
     public MotorCal winchR = new MotorCal(TypeMotor.SPARK, 0);
     public MotorCal winchL = new MotorCal(TypeMotor.SPARK, 0);
 
-    public boolean disabled;
 }
--- src/main/java/frc/robot/cals/SlappahCals.java
+++ src/main/java/frc/robot/cals/SlappahCals.java
@@ -4,7 +4,7 @@ import frc.robot.subsystems.motor.MotorCal;
 import frc.robot.subsystems.motor.MotorCal.TypeMotor;
 
 public class SlappahCals {
-    public boolean disable = false;
+    public boolean disable = true;
 
     public MotorCal angleMotor = new MotorCal(TypeMotor.SPARK, 0);
     public MotorCal transferMotor = new MotorCal(TypeMotor.SPARK, 0);
